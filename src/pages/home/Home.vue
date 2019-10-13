cb4c385f4c589d6638ae130c4b892365be2276ea
chen-mozi <3147028658@qq.com>
2019-10-13 14:24:04 +0800

    project init

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="home">home</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+.home {
+  font-size: 18px;
+  color: aquamarine;
+}
+</style>
