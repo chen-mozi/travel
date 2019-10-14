340efc9435eeb1844ce6ad54cacfb15abdb2da79
chen-mozi <3147028658@qq.com>
2019-10-15 00:16:19 +0800

    change

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of SwiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" /></swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      SwiperList: [{
+        id: '0001',
+        imgUrl: '//mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20199/387c5e767cccc7a3172ed7ea15b50727.png'
+      }, {
+        id: '0002',
+        imgUrl: '//mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20198/b59b8db9f72eb8f54df5bf2155672e9c.jpg'
+      }]
+    }
+  }
+}
+
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>>.swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom:  30.81%
+    background: #eee
+    .swiper-img
+       width 100%
+</style>
